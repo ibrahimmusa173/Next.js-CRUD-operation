0207e9514be766d9103bb4b3d09982f0d0d243fd
Musa Aman <musaaman705@gmail.com>
2026-05-02 22:00:25 +0500

    additional things removed in model

--- my-crud-app/server/models/Item.js
+++ my-crud-app/server/models/Item.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ItemSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    description: String,
-}, { timestamps: true });
+    
+}, );
 
 module.exports = mongoose.model('Item', ItemSchema);
